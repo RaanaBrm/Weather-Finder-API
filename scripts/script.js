a71af69df809d633efc64466a869391aea96dda3
Raana <raana.barimani@hyperisland.se>
2025-12-17 12:35:13 +0100

    fix the whole const for js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
 const API_KEY = "a887201f96aaf456017667a0680fad31";
 const LAST_CITY_KEY = "lastCity";
 
+const weatherBox = document.querySelector("#weatherBox");
+const statusBox = document.querySelector("#status");
+const cityInput = document.querySelector("#cityInput")
+const forecastBox = document.querySelector("#forecastBox");
+const cityNameEl = document.querySelector("#cityName");
+const temperatureEl = document.querySelector("#temperature");
+const descriptionEl = document.querySelector("#description");
+const humidityEl = document.querySelector("#humidity");
+const feelsLikeEl = document.querySelector("#feelsLike");
+const windEl = document.querySelector("#wind");
+const iconEl = document.querySelector("#weatherIcon");
+
 $(() => {
 
    $(`.overlay`).delay(6000).fadeOut();
